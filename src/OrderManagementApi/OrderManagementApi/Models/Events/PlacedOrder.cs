7fc6b40f3f94908625ab3984d1d960bb03e4ae3f
José Roberto Araújo <jroberto.araujo@gmail.com>
2022-01-20 23:48:53 +0000

    Removido projeto desatualizado

--- src/OrderManagementApi/OrderManagementApi/Models/Events/PlacedOrder.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OrderManagementApi.Models.Events
-{
-    public class PlacedOrder : IEvent
-    {
-        public int OrderId { get; set; }
-
-        public Dictionary<int, int> Items { get; set; }
-    }
-}
